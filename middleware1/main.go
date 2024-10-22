b4bc3158b6f01b075b6d2301a823fc778e51c46f
Cahlil Tillett <2021154337@ub.edu.bz>
2024-10-22 13:02:03 -0600

    commit

--- middleware1/main.go
+++ middleware1/main.go
@@ -2,15 +2,11 @@ package main
 
 import (
 	"fmt"
-	"log/slog"
 	"net/http"
 )
 
 func main() {
 	http.HandleFunc("/home", func(w http.ResponseWriter, r *http.Request) {
-		slog.Info("start", "method", r.Method, "path", r.URL.Path)
-		defer slog.Info("end", "method", r.Method, "path", r.URL.Path)
-
 		fmt.Fprintln(w, "hi im cahlil and i study information technology")
 	})
 
