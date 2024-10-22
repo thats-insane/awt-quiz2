91c10bf5d6b0f02e39b54337a7f546f034c767f9
Cahlil Tillett <2021154337@ub.edu.bz>
2024-10-21 18:57:33 -0600

    commit

---
+++ middleware1/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"log/slog"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/home", func(w http.ResponseWriter, r *http.Request) {
+		slog.Info("start", "method", r.Method, "path", r.URL.Path)
+		defer slog.Info("end", "method", r.Method, "path", r.URL.Path)
+
+		fmt.Fprintln(w, "hi im cahlil")
+	})
+
+	http.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request) {
+		slog.Info("start", "method", r.Method, "path", r.URL.Path)
+		defer slog.Info("end", "method", r.Method, "path", r.URL.Path)
+
+		fmt.Fprintln(w, "i study information technology")
+	})
+
+	http.ListenAndServe(":3000", nil)
+}
---
+++ middleware2/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"log/slog"
+	"net/http"
+)
+
+func logRequest(next func(http.ResponseWriter, *http.Request)) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		slog.Info("start", "method", r.Method, "path", r.URL.Path)
+		defer slog.Info("end", "method", r.Method, "path", r.URL.Path)
+
+		next(w, r)
+	}
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "hi im cahlil")
+}
+
+func aboutHander(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "i study information technology")
+}
+
+func main() {
+	http.HandleFunc("/home", logRequest(homeHandler))
+	http.HandleFunc("/about", logRequest(aboutHander))
+
+	http.ListenAndServe(":3000", nil)
+}
